3ffe1c93bd65133c9b34a72a38b338e8017fae69
rebecka-coder <reho0301@student.miun.se>
2020-02-03 15:19:10 +0100

    sending all files

---
+++ MyWebpage/wwwroot/js/main.js
@@ -0,0 +1,22 @@
+﻿"use strict";
+
+/*Hämta knappen*/
+var mybutton = document.getElementById("myBtn");
+
+
+/*Vid scrollning 20 px ned på sidan syns knappen*/
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+/* När användaren klickar på knappen, kommer man tillbaka upp på sidan*/
+function topFunction() {
+    document.body.scrollTop = 0; /* För Safari */
+    document.documentElement.scrollTop = 0; /* För Chrome, Firefox, IE och Opera*/
+}
